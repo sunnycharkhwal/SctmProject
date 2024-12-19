757879fd089b5ffe201bf855145b3718175de48e
Sunnny charkhwal <sunny.charkhwal@gmail.com>
2024-12-20 00:08:14 +0530

    add scss

--- Sctm/src/main.jsx
+++ Sctm/src/main.jsx
@@ -1,7 +1,8 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './scss/index.scss';
+import './scss/res.scss';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
